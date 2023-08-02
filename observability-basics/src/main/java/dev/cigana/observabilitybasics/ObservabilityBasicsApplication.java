3e8fcff68a5a42a7a3499c39fb7a0a6933444e88
João Cigana <joaocigana.cigana@gmail.com>
2023-08-02 14:21:47 -0300

    Commit inicial do projeto spring boot

---
+++ observability-basics/src/main/java/dev/cigana/observabilitybasics/ObservabilityBasicsApplication.java
@@ -0,0 +1,13 @@
+package dev.cigana.observabilitybasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservabilityBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservabilityBasicsApplication.class, args);
+	}
+
+}
---
+++ observability-basics/src/test/java/dev/cigana/observabilitybasics/ObservabilityBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cigana.observabilitybasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservabilityBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
